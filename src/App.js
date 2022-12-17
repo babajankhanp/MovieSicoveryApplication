ecf0023f65a3bb71af94e68002da4db511ccf7ca
babajankhanp <84612525+babajankhanp@users.noreply.github.com>
2022-12-17 20:56:12 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BoxModel from './RecentMovies';
+
+function App() {
+  return (
+    <div className="App">
+       <BoxModel/>
+    </div>
+  );
+}
+
+export default App;
